2cf5120d759f9852982b4b4ce2b3a1d4ce5c5853
CarterAndrewsITS <66387433+CarterAndrewsITS@users.noreply.github.com>
2021-02-24 15:45:01 -0800

    DeviceDriver class

--- azteca.py
+++ azteca.py
@@ -1,38 +1,12 @@
 
 import time
-import sys
-if sys.version_info[0] == 2:  # the tkinter library changed it's name from Python 2 to 3.
-    import Tkinter
-    tkinter = Tkinter #I decided to use a library reference to avoid potential naming conflicts with people's programs.
-else:
-    import tkinter
-from PIL import Image, ImageTk
 
-def showPIL(pilImage):
-    root = tkinter.Tk()
-    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-    root.overrideredirect(1)
-    root.geometry("%dx%d+0+0" % (w, h))
-    root.focus_set()    
-    root.bind("<Escape>", lambda e: (e.widget.withdraw(), e.widget.quit()))
-    canvas = tkinter.Canvas(root,width=w,height=h)
-    canvas.pack()
-    canvas.configure(background='black')
-    imgWidth, imgHeight = pilImage.size
-    if imgWidth > w or imgHeight > h:
-        ratio = min(w/imgWidth, h/imgHeight)
-        imgWidth = int(imgWidth*ratio)
-        imgHeight = int(imgHeight*ratio)
-        pilImage = pilImage.resize((imgWidth,imgHeight), Image.ANTIALIAS)
-    image = ImageTk.PhotoImage(pilImage)
-    imagesprite = canvas.create_image(w/2,h/2,image=image)
-    root.mainloop()
+
 
 
 #remove the most recent message from the queue
 def pop_message(client, url):
     response = client.receive_message(QueueUrl = url, MaxNumberOfMessages = 10)
-
     #last message posted becomes messages
     message = response['Messages'][0]['Body']
     receipt = response['Messages'][0]['ReceiptHandle']
@@ -44,8 +18,8 @@ client = getClient()
 queue_url = "https://sqs.us-west-1.amazonaws.com/277872029364/Azteca-Tasks"
 waittime = 1
 client.set_queue_attributes(QueueUrl = queue_url, Attributes = {'ReceiveMessageWaitTimeSeconds': str(waittime)})
-
-
+from DeviceDriver import DeviceDriver, ImageDriver
+driver=ImageDriver()
 while (True):
         #read from queue for new events every waittime
         try:
@@ -53,10 +27,8 @@ while (True):
                 message = pop_message(client, queue_url)
                 print(message)
                 if message == "Turn on projector":
-                        pilImage = Image.open(r"C:\Users\andre\Pictures\birthdayBackground.PNG")
-                        showPIL(pilImage)
+                        driver.turnOnProjectors()
                         #start projector
-                        projector='started'
                 #add other command interfaces here
                 time.sleep(waittime)
         #exit program
